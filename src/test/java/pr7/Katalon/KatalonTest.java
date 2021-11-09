59235468f6b4cc87bd66c90542a8143aa803660e
mrenatax <renatamansurova2001@gmail.com>
2021-11-09 19:40:43 +0300

    Katalon test

--- src/test/java/pr7/Katalon/KatalonTest.java
+++ src/test/java/pr7/Katalon/KatalonTest.java
@@ -1,20 +1,15 @@
-package pr7;
+package pr7.Katalon;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
 
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
 
 public class KatalonTest {
     private WebDriver driver;
@@ -55,39 +50,6 @@ public class KatalonTest {
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
         }
     }
 }
